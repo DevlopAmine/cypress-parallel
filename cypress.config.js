2025d9ac7f81a762882bc253033ab5b62e0d1e79
DevlopAmine <aminehouri47@gmail.com>
2024-03-01 16:44:43 +0100

    record2

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   projectId: "x2pvpo",
-  CYPRESS_RECORD_KEY: '46fec6e6-3734-4e43-a577-b74c4c88483e',
   e2e: {},
+  env: {
+    CYPRESS_RECORD_KEY: '46fec6e6-3734-4e43-a577-b74c4c88483e'
+  }
 }
 
-//Cypress.env('CYPRESS_RECORD_KEY', '46fec6e6-3734-4e43-a577-b74c4c88483e');
